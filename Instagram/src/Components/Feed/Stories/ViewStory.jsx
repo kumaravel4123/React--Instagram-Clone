d34cb27ad4bae5691d33e83f3908223fd9fa9fbd
kumaravel4123 <kumaravel3124@gmail.com>
2025-11-10 23:04:41 +0530

    edits

---
+++ Instagram/src/Components/Feed/Stories/ViewStory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ViewStory() {
+  return (
+    <div>ViewStory</div>
+  )
+}
+
+export default ViewStory
--- Instagram/src/main.jsx
+++ Instagram/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
+import React, { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ViewStory from './Components/Feed/Stories/ViewStory.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>
+  },
+  {
+    path:"/story",
+    element: <ViewStory/>
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <RouterProvider router={router} />
+  </StrictMode>
 )
+
+
