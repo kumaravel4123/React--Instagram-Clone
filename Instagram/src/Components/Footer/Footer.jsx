cb6f09f06e86a5af0cadf578deb8ba17c6621309
kumaravel4123 <kumaravel3124@gmail.com>
2025-12-13 19:30:43 +0530

    edits

--- Instagram/src/Components/Footer/Footer.jsx
+++ Instagram/src/Components/Footer/Footer.jsx
@@ -4,15 +4,14 @@ import "./Footer.css"
 function Footer() {
   return (
     <div>
-        <div className='Footer'>
-                    
-                    <div className='navs-h mt-2 ' ><i className="bi bi-house-door-fill"></i></div>
-                    <div className='navs' ><i className="bi bi-compass-fill"></i></div>
-                    <div className='navs' ><i className="bi bi-film"></i></div>
-                    <div className='navs' ><i className="bi bi-plus-lg"></i></div>
-                    <div className='navs ' ><i className="bi bi-send-fill"></i></div>
-                    <div className='navs me-3' onClick={()=>{navigate("/profile")}} ><i className="bi bi-person-circle"></i></div>
-                </div>
+      <div className='Footer'>
+        <a href=""> <div className='navs-h mt-2 ' ><i className="bi bi-house-door-fill"></i></div> </a>
+        <a href=""> <div className='navs' ><i className="bi bi-compass-fill"></i></div> </a>
+        <a href="">  <div className='navs' ><i className="bi bi-film"></i></div></a>
+        <a href="">  <div className='navs' ><i className="bi bi-plus-lg"></i></div></a>
+        <a href=""> <div className='navs ' ><i className="bi bi-send-fill"></i></div></a>
+        <a href="">  <div className='navs me-3' onClick={() => { navigate("/profile") }} ><i className="bi bi-person-circle"></i></div></a>
+      </div>
     </div>
   )
 }
--- Instagram/src/Components/Sidebar/Sidebar.jsx
+++ Instagram/src/Components/Sidebar/Sidebar.jsx
@@ -7,25 +7,27 @@ import { useNavigate } from 'react-router-dom';
 function Sidebar() {
 
     const navigate = useNavigate();
-    
+
     return (
         <>
-            <div className="m-3 position-fixed">
-                <div className='d-flex flex-column'>
-                    <img className='logo-image ' src="images\instagram-text.png" alt="" />
-                    <div className='navs-h' ><i className="bi bi-house-door-fill"></i> Home</div>
-                    <div className='navs' ><i className="bi bi-search"></i> Search</div>
-                    <div className='navs' ><i className="bi bi-compass-fill"></i> Explore</div>
-                    <div className='navs' ><i className="bi bi-film"></i> Reels</div>
-                    <div className='navs' ><i className="bi bi-send-fill"></i> Message</div>
-                    <div className='navs' ><i className="bi bi-bell-fill"></i> Notification</div>
-                    <div className='navs' ><i className="bi bi-plus-lg"></i> Create</div>
-                    <div className='navs' onClick={()=>{navigate("/profile")}} ><i className="bi bi-person-circle"></i> Profile</div>
-                </div>
-                <div className='d-flex flex-column position-fixed bottom-0 mb-3'>
-                    <div className='navs-b' ><i className="bi bi-list"></i> More</div>
-                    <div className='navs-b' ><i className="bi bi-ui-checks-grid"></i> Also from Meta</div>
-                    
+            <div className="sidebar">
+                <div className=" m-3 position-fixed ">
+                    <div className='d-flex flex-column'>
+                        <a href="#"> <img className='logo-image ' src="images\instagram-text.png" alt="" /> </a>
+                        <a href=""> <div className='navs-h' ><i className="bi bi-house-door-fill"></i> Home</div> </a>
+                        <a href=""> <div className='navs' ><i className="bi bi-search"></i> Search</div> </a>
+                        <a href=""> <div className='navs' ><i className="bi bi-compass-fill"></i> Explore</div> </a>
+                        <a href=""> <div className='navs' ><i className="bi bi-film"></i> Reels</div> </a>
+                        <a href=""> <div className='navs' ><i className="bi bi-send-fill"></i> Message</div> </a>
+                        <a href=""> <div className='navs' ><i className="bi bi-bell-fill"></i> Notification</div> </a>
+                        <a href=""> <div className='navs' ><i className="bi bi-plus-lg"></i> Create</div> </a>
+                        <a href="">  <div className='navs' onClick={() => { navigate("/profile") }} ><i className="bi bi-person-circle"></i> Profile</div> </a>
+                    </div>
+                    <div id='morebar' className=' d-flex flex-column position-fixed bottom-0 mb-3 '>
+                        <a href=""> <div className='navs-b' ><i className="bi bi-list"></i> More</div> </a>
+                        <a href=""> <div className='navs-b' ><i className="bi bi-ui-checks-grid"></i>From Meta</div> </a>
+
+                    </div>
                 </div>
             </div>
         </>
